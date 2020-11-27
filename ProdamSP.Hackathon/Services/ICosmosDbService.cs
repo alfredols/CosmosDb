0f5dd6a3c82e24c318e53ad54bb99366e3fbc539
p017377 <alfredolsantos@prodam.sp.gov.br>
2020-11-27 10:00:43 -0300

    add files

---
+++ ProdamSP.Hackathon/Services/ICosmosDbService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdamSP.Hackathon.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using ProdamSP.Hackathon.Models;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
